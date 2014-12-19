5193db04e8ee74b344d4b123a680d25241349ba2
Jose Contreras <jocontre@joselcontreras.com>
2014-12-18 23:00:23 -0600

    Files (not complete)

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+  <title>PHP Weather Scraper</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link href="style.css">
+  <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+  <div class="container">
+  	<div class="row">
+  	  <div class="col-md-6 col-md-offset-3 center">
+		<h1 class="center white">Weather Predictor</h1>
+
+		<p class="lead center white">Enter a city below to get a weather forecast.</p>
+
+		<form>
+		  <div class="form-group">
+		  <input type="text" class="form-control" name="city" id="city" placeholder="Eg. Lub-bick, Walla Walla, Lizard Lick..." /></div>
+		  <button id="findMyWeather" class="btn btn-success btn-lg">Get My Weather</button>
+		</form>
+
+		<div id="success" class="alert alert-success">Success</div>
+		<div id="fail" class="alert alert-danger">Could not find weather data for that city. Try again.</div>
+		<div id="noCity" class="alert alert-danger">Enter a city!</div>
+  	  </div>
+  	</div>
+  </div>
+
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  <script>
+
+    $("#findMyWeather").click(function(event) {
+      event.preventDefault(); // This prevents form from submitting
+
+      $(".alert").hide();
+
+      if ($("#city").val()!="") {
+      	$.get("scraper.php?city=" + $("#city").val(), function( data ) {
+
+      	  if (data=="") {
+      	  	$("#fail").fadeIn();
+      	  } 
+
+      	  else {
+      	  	$("#success").html(data).fadeIn();
+      	  }
+      	});
+	  } 
+
+	  else {
+	    $("#noCity").fadeIn();
+	  }
+    });
+  </script>
+</body>
+</html>
