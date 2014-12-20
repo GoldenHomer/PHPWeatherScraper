6e1df4cce16c8426c09dbd442b0ed7d58fdd5873
Jose Contreras <jocontre@joselcontreras.com>
2014-12-19 22:43:15 -0600

    Fixing scraper

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-A weather scraper in PHP. Will provide URL when I am done.
+A weather scraper in PHP. Can be found <a href="http://mythyme.co/WeatherScraper/index.php">here</a>. Not working yet. 
--- scraper.php
+++ scraper.php
@@ -1,8 +1,8 @@
 <?php
 
-$city = $_GET['city'];
+$city=$_GET['city'];
 
-$city = str_replace(" ", "", $city);
+$city=str_replace(" ", "", $city);
 
 $contents=file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 
