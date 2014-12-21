b58d7a9221ed58188e19361818f791367fd40c9c
Jose Contreras <jocontre@joselcontreras.com>
2014-12-20 23:20:55 -0600

    fix

--- scraper.php
+++ scraper.php
@@ -7,7 +7,7 @@ $city=str_replace(" ", "", $city);
 $contents=file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 
 preg_match('/3 Day Weather Forecast Summary:<\/b>
-	<span class="phrase">(.*?)</s', $contents, $matches); // <span class="phrase" found by element inspection
+	<span class="phrase">(.*?)</s', $contents, $matches); // <span class="phrase" found by element inspection on weather-forecast.com.
 
 echo $matches[1];
 ?>
