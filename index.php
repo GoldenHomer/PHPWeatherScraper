1fc197de43bdcec8cac9db4e585476b5024b44cd
Jose Contreras <jocontre@joselcontreras.com>
2014-12-24 13:45:32 -0600

    moving jquery to local

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
     });
 
   </script>
-  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="jquery-1.11.2.min.js"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
