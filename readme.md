813e65daf783bee0a49a8e50c99385f47aabb2eb
Jose Contreras <jocontre@joselcontreras.com>
2014-12-24 22:21:32 -0600

    Working now

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-A weather scraper in PHP. Can be found <a href="http://mythyme.co/WeatherScraper/index.php">here</a>. Not working yet. 
+A weather scraper in PHP. Can be found <a href="http://mythyme.co/WeatherScraper/index.php">here</a>.
--- scraper.php
+++ scraper.php
@@ -6,8 +6,7 @@ $city=str_replace(" ", "", $city);
 
 $contents=file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
 
-preg_match('/3 Day Weather Forecast Summary:<\/b>
-	<span class="phrase">(.*?)</s', $contents, $matches); // <span class="phrase" found by element inspection on weather-forecast.com.
+preg_match('/3 Day Weather Forecast Summary:(.*?)<span class="phrase">(.*?)</s', $contents, $matches); // <span class="phrase" found by element inspection on weather-forecast.com.
 
-echo $matches[1];
+echo $matches[2];
 ?>
